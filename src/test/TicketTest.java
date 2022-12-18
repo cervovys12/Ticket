c0f1ab518e0d03fa4663db43e54e1f733305e9e6
cervovys12 <ssnmoumen@gmail.com>
2022-12-18 14:30:45 +0100

    show item informations

--- src/test/TicketTest.java
+++ src/test/TicketTest.java
@@ -1,10 +1,23 @@
 package test;
 
+import main.Item;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class TicketTest {
+    private static final double DELTA = 1e-15;
+
     @Test
     public void HelloWorld() {
        System.out.println("Hello World");
+    }
+    @Test
+    public void showItemInformation() {
+        float prixUnitaire = 3;
+        int quantite = 2;
+        Item item = new Item("Tacos", quantite, prixUnitaire);
+        assertEquals(item.getPrixTotal(),quantite * prixUnitaire,DELTA);
+        System.out.println(item.toString());
     }
 }

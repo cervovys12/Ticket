31a929a794a7f0276f91a5cb8039b121cdb9d7e5
lelou96 <petelhoundiallo@gmail.com>
2022-12-21 21:43:10 +0100

    show reduction List

---
+++ src/main/Commande.java
@@ -0,0 +1,20 @@
+package main;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Commande {
+
+	
+	public List<Integer> getReductionList()
+	{
+		List<Integer> reductionList = new ArrayList<>();
+		reductionList.add(3);
+		reductionList.add(5);
+		reductionList.add(7);
+		reductionList.add(10);
+		reductionList.add(15);
+		return reductionList;
+	}
+}
--- src/test/TicketTest.java
+++ src/test/TicketTest.java
@@ -2,9 +2,17 @@ package test;
 
 import org.junit.Test;
 
+import main.Commande;
+
 public class TicketTest {
-    @Test
-    public void HelloWorld() {
-       System.out.println("Hello World");
-    }
+
+	@Test
+	public void showReductionList() {
+		System.out.println("ID 11\n LISTE DES REDUCTIONS POSSIBLES");
+		Commande commande = new Commande();
+		for (Integer reduction : commande.getReductionList()) {
+			System.out.println(reduction + "%");
+		}
+	}
+
 }
